b89e4f7631e2e3229c54fdcfb1c7b1e6ae03f680
Greg Borelly <gregborrelly@gmail.com>
2017-03-01 20:55:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 
+var FederalTables = {
+    /*
+        This Object models the federal Tax tables
+        for a weekly paycheck. Each element on each 
+        array correspond to a row on the tax tables. 
+    
+    */  
+    minimums : [0,44,224,774,1812,3730,8058,8090],
+    maximums : [44,224,774,1812,3730,8058,8090,8090],
+    discounts : [0,4.40,15.60,93,147.36,333.86,495.02,867.16],
+    percents : [.0,.10,.15,.25,.28,.33,.35,.396],  
+    taxTableLength: 7
+        
+};
+
+
 var paycheck = {
     amount : 1000,
-    biweekly: true,
+    biweekly: false,
     monthly: false,
     status: "single", 
-    /*
+   
+    federalTax:function() {
+       /*
         The federal tax function, loops through each tax table
         comparing the amount earned to each possible tax bracket. 
     
-    */
+       */
     
-    federalTax:function() {
-        var i = minimums.length-1;
-        var minimums = [0,44,224,774,1812,3730,8058,8090];
-        var maximums = [44,224,774,1812,3730,8058,8090,8090];
-        var discounts = [0,4.40,15.60,93,147.36,333.86,495.02,867.16];
-        var percents = [.0,.10,.15,.25,.28,.33,.35,.396];
-        
+        var i = FederalTables.taxTableLength;
     
         while( i > -1){
-            if(this.amount < maximums[i] && this.amount > minimums[i]){
+            if(this.amount < FederalTables.maximums[i] && this.amount > FederalTables.minimums[i]){
             
-             var tax = this.amount * percents[i];
-             return tax- discounts[i];
+             var tax = this.amount * FederalTables.percents[i];
+             return tax- FederalTables.discounts[i];
             } else{
                 
                  i= i - 1;
@@ -49,3 +61,4 @@ if (paycheck.biweekly === paycheck.monthly){
 paycheck.amount = 500;
 console.log(paycheck.federalTax());
 
+document.querySelector("#income").textContent = paycheck.federalTax();
